e45adeeae9fd7dab64c99df9f29781208a5e1a3d
G4meD3v <mrwpgg@gmail.com>
2024-11-23 22:49:05 +0330

    fix unregistered player

--- Source/Gear/GameFramework/LobbyGameState.cpp
+++ Source/Gear/GameFramework/LobbyGameState.cpp
@@ -8,10 +8,36 @@ void ALobbyGameState::AddPlayerState(APlayerState* PlayerState)
 {
 	Super::AddPlayerState(PlayerState);
 
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		APlayerController* const PlayerController = Iterator->Get();
+		if (PlayerController && PlayerController->IsLocalController())
+		{
+			ALobbyPlayerController* LobbyController = Cast<ALobbyPlayerController>(PlayerController);
+
+			if (IsValid(LobbyController))
+			{
+				LobbyController->NotifyNewPlayer(PlayerState);
+			}
+		}
+	}
 }
 
 void ALobbyGameState::RemovePlayerState(APlayerState* PlayerState)
 {
 	Super::RemovePlayerState(PlayerState);
 
+	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+	{
+		APlayerController* const PlayerController = Iterator->Get();
+		if (PlayerController && PlayerController->IsLocalController())
+		{
+			ALobbyPlayerController* LobbyController = Cast<ALobbyPlayerController>(PlayerController);
+
+			if (IsValid(LobbyController))
+			{
+				LobbyController->NotifyRemovePlayer(PlayerState);
+			}
+		}
+	}
 }
--- Source/Gear/GameFramework/LobbyPlayerState.cpp
+++ Source/Gear/GameFramework/LobbyPlayerState.cpp
@@ -8,40 +8,14 @@ void ALobbyPlayerState::BeginPlay()
 {
 	Super::BeginPlay();
 
-	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
-	{
-		APlayerController* const PlayerController = Iterator->Get();
-		if (PlayerController && PlayerController->IsLocalController())
-		{
-			ALobbyPlayerController* LobbyController = Cast<ALobbyPlayerController>(PlayerController);
-
-			if (IsValid(LobbyController))
-			{
-				LobbyController->NotifyNewPlayer(this);
-			}
-		}
-	}
-
 
 }
 
 void ALobbyPlayerState::Destroyed()
 {
-	for (FConstPlayerControllerIterator Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
-	{
-		APlayerController* const PlayerController = Iterator->Get();
-		if (PlayerController && PlayerController->IsLocalController())
-		{
-			ALobbyPlayerController* LobbyController = Cast<ALobbyPlayerController>(PlayerController);
-
-			if (IsValid(LobbyController))
-			{
-				LobbyController->NotifyRemovePlayer(this);
-			}
-		}
-	}
-
 	Super::Destroyed();
+
+
 }
 
 void ALobbyPlayerState::OnRep_PlayerName()
